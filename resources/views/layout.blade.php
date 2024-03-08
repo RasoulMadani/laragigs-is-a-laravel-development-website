6f473eaf98b4fb412d94b82cb9da97cd3f00d515
Rasoulmadani <ali.rahbary@chmail.ir>
2024-03-08 12:19:40 +0330

    create blade file

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,21 @@
+<!--
+/* ﷲ  ﷽  ﷲ  ﷽   ﷽  ﷲ  ﷽  ﷲ  ﷽  ﷲ  ﷽  ﷲ  ﷽  ﷲ.. 
+|:          **** بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ ****           :|
+|:     ⚡☝..✍-☝..✍-☝..✍-☝..✍-☝..✍-☝..✍⚡   :|
+|:               ⌚ in the name of allah ⌛              :|
+﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽*/
+-->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="" />
+        <title>allah - laragigs</title>
+    </head>
+    <body>
+        <h1>Laragigs</h1>
+        {{-- VIEW OUTPUT --}}
+        @yield('content')
+    </body>
+</html>
